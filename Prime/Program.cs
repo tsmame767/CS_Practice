0097f58543dca47f5fc710c7490fa3c3b28966e3
tsmame767 <tsmane789@gmail.com>
2024-01-15 12:06:30 +0530

    first code

---
+++ Prime/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+int sum = 0;
+for(int i = 1; i < 101; i++)
+{
+    sum = 0;
+    for (int j = 1; j < i+1; j++)
+    {
+        if (i % j == 0)
+        {
+            sum += 1;
+        }
+    }
+    if (sum == 2)
+    {
+        Console.WriteLine(i);
+
+    }
+}
