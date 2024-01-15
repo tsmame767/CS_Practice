c7860b5e69a203f46b5b92d39a521cff48fce389
tsmame767 <tsmane789@gmail.com>
2024-01-15 12:19:33 +0530

    new task

---
+++ evenrange/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+for(int i = 0; i < 101; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+
+}
